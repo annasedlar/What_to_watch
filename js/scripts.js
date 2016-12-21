87a964aee07576c853f2b4aa9e1d8cf5651a310c
Anna Sedlar <annasedlar@gmail.com>
2016-12-21 11:05:50 -0500

    now playing posters

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
-
-
 $(document).ready(function(){
 	// the base url for all api calls
 	var apiBaseUrl = 'http://api.themoviedb.org/3/';
 	var imageBaseUrl = 'http://image.tmdb.org/t/p/';
-	const nowPlayingUrl = apiBaseUrl + "movies/now_playing";
+	const nowPlayingUrl = apiBaseUrl + "movie/now_playing?api_key="+apiKey;
+
+	$.getJSON(nowPlayingUrl, function(nowPlayingData){
+		console.log(nowPlayingData);
+		var nowPlayingHTML = '';
+		for(let i=0; i< nowPlayingData.results.length; i++){
+			var poster = imageBaseUrl+'w300'+nowPlayingData.results[i].poster_path
+			console.log(poster);
+			nowPlayingHTML += '<div class="col-sm-3">';
+				nowPlayingHTML += '<img src="' + poster+ '">'
+				nowPlayingHTML += '</div>'; 
+			}
+				$('#movie-grid').html(nowPlayingHTML);
+		
 
 
-	// https://api.themoviedb.org/3/movie/76341?api_key=5dcf2565edd78552945798ebbbd87710
+
+	})
+
+
 })
