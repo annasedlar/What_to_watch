8bd0f28d3cf7817d2e003e0f928557c84954e2c5
Anna Sedlar <annasedlar@gmail.com>
2016-12-21 10:06:04 -0500

    config with api key and gitignore

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log("test");
+
+
+$(document).ready(function(){
+	// the base url for all api calls
+	var apiBaseUrl = 'http://api.themoviedb.org/3/';
+	var imageBaseUrl = 'http://image.tmdb.org/t/p/';
+	const nowPlayingUrl = apiBaseUrl + "movies/now_playing";
+
+
+	// https://api.themoviedb.org/3/movie/76341?api_key=5dcf2565edd78552945798ebbbd87710
+})
